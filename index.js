4a4ffd40eacb0fcff841e5b4a74c2aa50b0b2e33
Anusha <anushasunil71201@gmail.com>
2021-09-14 02:14:56 +0530

    updated validation function

--- index.js
+++ index.js
@@ -3,29 +3,47 @@ const checkButton = document.querySelector(".btn-check");
 const cashGiven = document.querySelector("#bill-received");
 const message = document.querySelector("#error-message");
 var noOfNotes = document.querySelectorAll(".no-of-notes");
+const nextButton = document.querySelector(".btn-next");
+const validMessage = document.querySelector(".validate-bill-amount")
+const billValidSection = document.querySelector(".bill-amount-validation")
 
+billValidSection.style.display = "none";
 
-checkButton.addEventListener("click", function validateBillandCashAmount(){
+nextButton.addEventListener("click", function validateBillAmount(){
 
-    hideMessage()
-    if(billAmount.value > 0)
+    if(Number(billAmount.value))
     {
-        if(Number(cashGiven.value) >= Number(billAmount.value))
+        if(billAmount.value > 0)
         {
-            const amountToBeReturned = cashGiven.value - billAmount.value;
-            showMessage("Thankyou for the payment. 🙂");
-            calculateChange(amountToBeReturned);
+            billValidSection.style.display = "block";
+            hideValidationMessage()
+            checkButton.addEventListener("click", function validateCashAmount(){
+               
+                hideMessage()
+                if(Number(cashGiven.value) >= Number(billAmount.value))
+                {
+                    const amountToBeReturned = cashGiven.value - billAmount.value;
+                    showMessage("Thankyou for the payment. 🙂");
+                    calculateChange(amountToBeReturned);
+                }
+                else
+                {
+                    showMessage("Do you wanna wash plates? 🤨.");
+                }
+            })
+            
         }
         else
         {
-            showMessage("Do you wanna wash plates? 🤨.");
+            validationMessage("The bill amount should be greater than 0.");
         }
-    } 
+    }
     else
     {
-        showMessage("The bill amount should be greater than 0.");
+        validationMessage("The values entered should be a number.")
     }
 });
+
 
 function calculateChange(amt)
 {
@@ -44,8 +62,27 @@ function hideMessage(){
     message.style.display = "none"
 }
 
+function hideValidationMessage(){
+    validMessage.style.display = "none"
+}
+
 function showMessage(msg)
 {
     message.style.display = "block";
+    message.style.marginLeft = '1rem';
+    message.style.color = 'var(--textColor)';
+    message.style.fontSize = '1.2rem';
+    message.style.paddingTop = "1rem";
+    message.style.paddingBottom = "1rem";
     message.innerText = msg;
+}
+
+function validationMessage(msg){
+    validMessage.style.display = "block";
+    validMessage.style.marginLeft = '1rem';
+    validMessage.style.color = 'var(--textColor)';
+    validMessage.style.fontSize = '1.2rem';
+    validMessage.style.paddingTop = "1rem";
+    validMessage.style.paddingBottom = "1rem";
+    validMessage.innerText = msg;
 }
