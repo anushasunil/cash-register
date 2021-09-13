d69a5a213b3311ba320a055b00ccae41b9b7b509
Anusha <anushasunil71201@gmail.com>
2021-09-13 21:50:01 +0530

    docs: updated validate function

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
 const billAmount = document.querySelector("#bill-amount");
 const checkButton = document.querySelector(".btn-check");
 const cashGiven = document.querySelector("#bill-received");
+const message = document.querySelector("#error-message");
+
+
+checkButton.addEventListener("click", function validateBillandCashAmount(){
+
+    hideMessage()
+    if(billAmount.value > 0)
+    {
+        console.log("amount received", cashGiven.value)
+        console.log("amount to be paid", billAmount.value)
+        console.log(cashGiven.value - billAmount.value)
+        if(Number(cashGiven.value) >= Number(billAmount.value))
+        {
+            const amountToBeReturned = cashGiven.value - billAmount.value;
+            showMessage("");
+            calculateChange(amountToBeReturned);
+            console.log("here all normal")
+        }
+        else
+        {
+            showMessage("Do you wanna wash plates? 🤨.");
+        }
+    } 
+    else
+    {
+        showMessage("The bill amount should be greater than 0.");
+    }
+});
+
+
+function hideMessage(){
+    message.style.display = "none"
+}
+
+function showMessage(msg)
+{
+    message.style.display = "block";
+    message.innerText = msg;
+}
